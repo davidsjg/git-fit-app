79d6d134e45263007a7751620f6470d6fc7858fa
Joey Davidson <j.davidson18@gmail.com>
2021-04-01 14:34:21 -0600

    push to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3050;
 
 const app = express();
 
